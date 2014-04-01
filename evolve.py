844a1dabafa75e79c402726c331ddf8c1df5eaa9
Adam Simpson <atj@atjlt2.ornl.gov>
2014-04-01 14:58:31 -0400

    Fixed deadlock bug caused by initial parents not being identical

--- evolve.py
+++ evolve.py
@@ -6,7 +6,6 @@ from polygon import Polygon
 import time
 
 def main():
-
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     size = comm.Get_size()
@@ -15,45 +14,50 @@ def main():
     master_image = Image.open("master.png").convert(mode="RGB")   
     width, height = master_image.size
 
-    # Create initial polygons
-    polygons = []
-    for i in range(200):
-        x_points = np.random.random_integers(low=0,high=width,size=3).tolist()
-        y_points = np.random.random_integers(low=0,high=height,size=3).tolist()
-        points = zip(x_points, y_points)
-        color = tuple(np.random.random_integers(low=0, high=255, size=4).tolist())
-        polygon = Polygon(points, color)
-        polygons.append(polygon)
+    # Create initial polygons on rank 0 and distribute
+    # Algorithm assumes parent is the same on all ranks
+    if rank == 0:
+        polygons = []
+        for i in range(200):
+            x_points = np.random.random_integers(low=0,high=width,size=3).tolist()
+            y_points = np.random.random_integers(low=0,high=height,size=3).tolist()
+            points = zip(x_points, y_points)
+            color = tuple(np.random.random_integers(low=0, high=255, size=4).tolist())
+            polygon = Polygon(points, color)
+            polygons.append(polygon)
+    else:
+        polygons = None
+    
+    # Brodcast initial polygon data
+    polygons = comm.bcast(polygons, root=0)
 
     # Create initial DNA
     parent = DNA(polygons, master_image)
 
     # Evolve DNA and breed fittest
-    for i in range(1000000):
+    for i in range(1000):
         # Create child
         child = parent.breed()
 
         # Gather all ranks fitnesses
-        fitness = child.fitness
-        fitnesses = comm.allgather(fitness)
+        fitnesses = comm.allgather(child.fitness)
 
         # Find rank with minimum fitness
-        min_fitness = min(fitnesses)
+        best_fitness = min(fitnesses)
+        best_rank = fitnesses.index(best_fitness)
 
         # If lowest child lower than current parent scatter child
-        if min_fitness < parent.fitness:
-            best_child_rank = fitnesses.index(min_fitness)
-
+	if best_fitness < parent.fitness:
             # Set polygon data to be sent
-            if best_child_rank == rank:
+            if best_rank == rank:
                 polygons = child.polygons
             else:
                 polygons = None
 
             # Brodcast best polygon data
-            polygons = comm.bcast(polygons, root=best_child_rank)
+            polygons = comm.bcast(polygons, root=best_rank)
 
-            if best_child_rank == rank:
+            if best_rank == rank:
                 parent = child
             else:
                 parent = DNA(polygons, master_image)
@@ -61,6 +65,7 @@ def main():
         # Save image and polygon information
         if i%1000 == 0 and rank == 0:
             parent.save()
+            print len(parent.polygons), "polygons saved"
 
 if __name__ == "__main__":
     main()
